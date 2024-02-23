b8fb766b75996361c39b08a41b9a65fa6524c6dd
Jacob Jayme <jaymerjacob@Jacobs-MacBook-Air.local>
2024-02-23 12:43:18 -0800

    Added Iterating over a list of posts for the application

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template
 
 app = Flask(__name__)
 
+posts=[{"post-liker":"Lanceyboi","name":"Maria","profile-picture":"assets/maria1.jpg", "pictures":["assets/maria1.jpg","assets/maria2.jpg","assets/maria3.jpg"], "description":"Lance Likes Guys", "Likes":"80", "post-datetime":"2/2/2024"}]
+
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html',posts=posts)
