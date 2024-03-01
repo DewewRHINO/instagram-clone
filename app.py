870a680cfef6a48e8da3b9c78d2d6884fb92bc9a
DewewRHINO <jrjayme@cpp.edu>
2024-03-01 11:24:37 -0800

    Added Lance as a villain

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 
 posts=[
     {
+        "post-liker":"Aaron Minh",
+        "name":"Lance Shinigawa",
+        "profile-picture":"assets/villain1.jpg", 
+        "pictures":["assets/villain1.jpg"], 
+        "description":"I am the funniest mentor", 
+        "Likes":"2 People 🥺", 
+        "post-datetime":"8/2/2023"
+    }, 
+    {
         "post-liker":"Lancey Shinigawa",
         "name":"Kyle Rodriguez",
         "profile-picture":"assets/kyle1.png", 
@@ -14,14 +23,42 @@ posts=[
         "post-datetime":"2/2/2024"
     }, 
     {
+        "post-liker":"Minh Duy Tran",
+        "name":"Lance Shinigawa",
+        "profile-picture":"assets/villain1.jpg", 
+        "pictures":["assets/villain6.jpg"], 
+        "description":"I wasn't always a villain", 
+        "Likes":"20k", 
+        "post-datetime":"8/2/2023"
+    },
+    {
         "post-liker":"Lancey Shinigawa",
         "name":"Kyle Rodriguez",
         "profile-picture":"assets/kyle1.png", 
-        "pictures":["assets/kyle1.png","assets/kyle2.png","assets/kyle3.png"], 
+        "pictures":["assets/kyphoto1.jpg","assets/kyphoto2.jpg"], 
         "description":"Checkout my new photography website! I'll be taking orders 😉.", 
         "Likes":"40 Million", 
         "post-datetime":"8/2/2023"
-    } 
+    }, 
+    {
+        "post-liker":"Ashley Minh",
+        "name":"Lance Shinigawa",
+        "profile-picture":"assets/villain1.jpg", 
+        "pictures":["assets/villain3.jpg"], 
+        "description":"I'm so funny", 
+        "Likes":"No one", 
+        "post-datetime":"1 Day ago"
+    }, 
+    {
+        "post-liker":"Ashley Minh",
+        "name":"Lance Shinigawa",
+        "profile-picture":"assets/villain9.jpg", 
+        "pictures":["assets/villain9.jpg"], 
+        "description":"Met my idol today!", 
+        "Likes":"No one", 
+        "post-datetime":"5/27/2022"
+    }
+
 ]
 
 @app.route("/")
