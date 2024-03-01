d620fb894f634e03e1c43414a121b658e7d25fc4
DewewRHINO <jrjayme@cpp.edu>
2024-03-01 10:27:47 -0800

    Cleaned pictures

--- app.py
+++ app.py
@@ -3,7 +3,26 @@ from flask import render_template
 
 app = Flask(__name__)
 
-posts=[{"post-liker":"Lanceyboi","name":"Maria","profile-picture":"assets/maria1.jpg", "pictures":["assets/maria1.jpg","assets/maria2.jpg","assets/maria3.jpg"], "description":"Lance Likes Guys", "Likes":"80", "post-datetime":"2/2/2024"}]
+posts=[
+    {
+        "post-liker":"Lancey Shinigawa",
+        "name":"Kyle Rodriguez",
+        "profile-picture":"assets/kyle1.png", 
+        "pictures":["assets/kyle1.png","assets/kyle2.png","assets/kyle3.png"], 
+        "description":"I love taking pictures, taking pictures is so fun.", 
+        "Likes":"20 Million", 
+        "post-datetime":"2/2/2024"
+    }, 
+    {
+        "post-liker":"Lancey Shinigawa",
+        "name":"Kyle Rodriguez",
+        "profile-picture":"assets/kyle1.png", 
+        "pictures":["assets/kyle1.png","assets/kyle2.png","assets/kyle3.png"], 
+        "description":"Checkout my new photography website! I'll be taking orders 😉.", 
+        "Likes":"40 Million", 
+        "post-datetime":"8/2/2023"
+    } 
+]
 
 @app.route("/")
 def hello_world():
