04e25f1be76118feba092c5cf82d3f0fce52ebc8
DewewRHINO <jrjayme@cpp.edu>
2024-03-04 16:47:18 -0800

    Added more posts: Josh and Maria

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ posts=[
         "name":"Kyle Rodriguez",
         "profile-picture":"assets/kyle1.png", 
         "pictures":["assets/kyle1.png","assets/kyle2.png","assets/kyle3.png"], 
-        "description":"I love taking pictures, taking pictures is so fun.", 
+        "description":"I love taking pictures, taking pictures is so fun. All started back in my Cypress High School Days. What a time.", 
         "Likes":"20 Million", 
         "post-datetime":"2/2/2024"
     }, 
@@ -36,7 +36,7 @@ posts=[
         "name":"Kyle Rodriguez",
         "profile-picture":"assets/kyle1.png", 
         "pictures":["assets/kyphoto1.jpg","assets/kyphoto2.jpg"], 
-        "description":"Checkout my new photography website! I'll be taking orders 😉.", 
+        "description":"Checkout my new photography website! I'll be taking orders 😉. Just started this company today! You can visit it at https://dewewrhino.github.io/kyphoto/", 
         "Likes":"40 Million", 
         "post-datetime":"8/2/2023"
     }, 
@@ -57,8 +57,35 @@ posts=[
         "description":"Met my idol today!", 
         "Likes":"No one", 
         "post-datetime":"5/27/2022"
+    },
+    {
+        "post-liker":"Lance Minh",
+        "name":"The Legend",
+        "profile-picture":"assets/MINHDUYTRAN.png", 
+        "pictures":["assets/MINHDUYTRAN.png"], 
+        "description":"I am Minh, I am very epic. (ignore the rest of this, it's for lance): MDAgMSAxIDAgMDAgMSAxMCAwIDExIDAxIDAxIDAwMCAxMTAgMCAwIDAwMCAwMTEgMTEgMTExIDAxMDEgMTEwIDExMSAxMTEgMDEwMCAwMDAgMTEwIDEwMSAwMTEgMSAwMCAwMDAgMCAxMSAwMDAgMDE=", 
+        "Likes":"20 Billion", 
+        "post-datetime":"5/13/2022"
+    },
+    {
+        "post-liker":"Lance Minh",
+        "name":"Maria",
+        "profile-picture":"assets/maria1.jpg", 
+        "pictures":["assets/maria1.jpg, assets/maria2.jpg, assets/maria3.jpg, assets/maria4.jpg"], 
+        "description":"Had a great time at the aquarium in Long Beach!!! Saw this really epic scorpion.", 
+        "Likes":"100 Billion", 
+        "post-datetime":"3/1/2024"
+    }, 
+    {
+        "post-liker":"Maria",
+        "name":"Josh",
+        "profile-picture":"assets/mfa.jpg", 
+        "pictures":["mfa.jpg"], 
+        "description":"Just got this MFA token today! Very epic.", 
+        "Likes":"50 Billion", 
+        "post-datetime":"3/1/2024"
     }
-
+    
 ]
 
 @app.route("/")
