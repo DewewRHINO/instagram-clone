4d18eab0cfd134571430a587a944394a407da0a2
DewewRHINO <jrjayme@cpp.edu>
2024-03-04 21:13:01 -0800

    Added the right id card with bar code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ posts=[
         "post-liker":"Aaron Minh",
         "name":"Lance Shinigawa",
         "profile-picture":"assets/villain1.jpg", 
-        "pictures":["assets/villain1.jpg", "assets/oopsec.jpg"], 
+        "pictures":["assets/villain1.jpg", "assets/oopsec.jpg", "assets/1.png"], 
         "description":"I am the funniest mentor, no wonder Minh hired me for Minhtendo. Had a great first day today, my ID card looking dapper. Checkout my blog where I talk about why I am such a villain: https://dewewrhino.github.io/lance-is-a-villain/", 
         "Likes":"2 People 🥺", 
         "post-datetime":"8/2/2023"
